74d22b0ae23bdcbd5cd620a929ac058d32586e8d
eggyo <eggyoyo4859@gmail.com>
2017-02-08 10:35:43 +0700

    v.1

--- index.js
+++ index.js
@@ -64,8 +64,8 @@ app.get('/loadGoogleMapImage/center=:lat,:lon&zoom=:zoom&gridCount=:gridCount', 
         Jimp.read(url).then(function (image) {
           // do stuff with the image
           console.log("image : " +image);
-          image.crop( 0, 60, 1160, 1160).write(__dirname + './map.png');         // crop to the given region
-          response.sendFile(__dirname + './map.png');
+          image.crop( 0, 60, 1160, 1160).write(__dirname + '/map.png');         // crop to the given region
+          response.sendFile(path.resolve('map.png'));
         }).catch(function (err) {
           console.log("image err: " +err);
         });
