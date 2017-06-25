6a4d2ee106081e8e1b4b4b8cf5a5438777007975
eggyo <eggyoyo4859@gmail.com>
2017-06-25 11:04:50 +0700

    v.1

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/startcrawer', function(req, res) {
     const $ = cheerio.load(body);
     var test=[];
     $('div[id=wizard]').children().each(function(i, elem) {
-      var test[i] = $(this).children('.article').text();
+      test[i] = $(this).children('.article').text();
       console.log('------->test:'+i+':'+ test[i]); // Print the HTML for the Google homepage.
 
     });
